bd82995156e64201533b163bf8c2c9230bc76edc
Om Sharma <oms177794@gmail.com>
2026-06-21 09:21:56 +0530

    Added Comments for Understanding

--- day67-68.jsx
+++ day67-68.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div>
-      {/* Button to show/hide the message */}
+      
       <button
         onClick={() =>
           // Toggle the value between true and false
