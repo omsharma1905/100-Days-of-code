6469322b10a0d4cf15f013d406d2bfe4d248a228
Om Sharma <oms177794@gmail.com>
2026-06-23 09:45:53 +0530

    Added Comments for Understanding

--- day61-62.jsx
+++ day61-62.jsx
@@ -1,17 +1,27 @@
 // App.js
+
+// Import React library
 import React from "react";
 
+// Greeting component receives props as an argument
 function Greeting(props) {
-  return <h1>Hello, {props.name}!</h1>;
+  return (
+    // Display a greeting message using the name prop
+    <h1>Hello, {props.name}!</h1>
+  );
 }
 
 function App() {
   return (
     <div>
+      {/* Passing "World" as the name prop */}
       <Greeting name="World" />
+
+      {/* Passing "Developer" as the name prop */}
       <Greeting name="Developer" />
     </div>
   );
 }
 
+// Export App component so it can be used in other files
 export default App;
